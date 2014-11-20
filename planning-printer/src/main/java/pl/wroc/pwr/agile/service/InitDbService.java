d6a19bfcf0e69b005a3e2df6be1d7252ad39eb13
maciekradoszko@gmail.com <maciekradoszko@gmail.com@844e95ce-14d9-20eb-99b5-a5bf619f410c>
2014-11-20 22:32:24 +0000

    szyfrowanie

--- planning-printer/src/main/java/pl/wroc/pwr/agile/service/InitDbService.java
+++ planning-printer/src/main/java/pl/wroc/pwr/agile/service/InitDbService.java
@@ -21,8 +21,7 @@ public class InitDbService {
         user1.setName("Bugi");
         user1.setEmail("bugi@pwr.edu.pl");
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        //user1.setPassword(encoder.encode("bugi"));
-        user1.setPassword("bugi");
+        user1.setPassword(encoder.encode("bugi"));
         userRepository.save(user1);
     }
 }
