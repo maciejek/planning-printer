c40f297e31871edad669b906077fb4620eb1dbdf
maciekradoszko <maciekradoszko@gmail.com>
2014-11-20 20:57:00 +0000

    after video 28 - time for zastepca of scrum master

---
+++ planning-printer/src/main/java/pl/wroc/pwr/agile/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.agile.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.wroc.pwr.agile.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+    User findByName(String name);
+
+}
