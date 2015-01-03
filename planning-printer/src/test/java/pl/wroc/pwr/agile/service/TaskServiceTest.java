54dd15af378ba4f5a646e3f31144368df5f3b8cd
wposlednicka <wposlednicka@844e95ce-14d9-20eb-99b5-a5bf619f410c>
2015-01-03 18:28:53 +0000

    poprawa testów userStory i task

--- planning-printer/src/test/java/pl/wroc/pwr/agile/service/TaskServiceTest.java
+++ planning-printer/src/test/java/pl/wroc/pwr/agile/service/TaskServiceTest.java
@@ -1,8 +1,10 @@
 package pl.wroc.pwr.agile.service;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +13,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 import pl.wroc.pwr.agile.entity.Task;
 import pl.wroc.pwr.agile.entity.User;
+import pl.wroc.pwr.agile.entity.UserStory;
 import pl.wroc.pwr.agile.entity.Workspace;
 import pl.wroc.pwr.agile.repository.TaskRepository;
 
 public class TaskServiceTest {
 	
 	private static final int TASK_ID = 99;
-	private static final String NUMBER = "US01-test";
-	private static final String POINTS = "5";
-	private static final String SUMMARY = "Testowy opis user story";
+	private static final String NUMBER = "T01-test";
+	private static final String POINTS = "3";
+	private static final String SUMMARY = "Testowy task";
+	
+	@Mock
 	private Task task;
 	
 	@InjectMocks
@@ -61,7 +65,11 @@ public class TaskServiceTest {
 	@Test
 	public void shouldGetTaskById(){
 		taskService.saveTask(task);
-		
+		verify(taskRepository).save(task);
+		taskService.getTaskById(task.getId());
+		verify(taskRepository).findOne(task.getId());
+		taskService.deleteTaskById(task.getId());
+		verify(taskRepository).delete(task.getId());
 	}
 
 }
