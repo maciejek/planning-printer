3bdbae39e1b6af8669a76f97f7dd940a3dd6fcc6
wposlednicka <wposlednicka@844e95ce-14d9-20eb-99b5-a5bf619f410c>
2015-01-06 14:26:29 +0000

    poprawa pdf - generuje się już plik pdf - jedynie wygląd do poprawy

--- planning-printer/src/main/java/pl/wroc/pwr/agile/controller/WorkspaceController.java
+++ planning-printer/src/main/java/pl/wroc/pwr/agile/controller/WorkspaceController.java
@@ -3,7 +3,9 @@ package pl.wroc.pwr.agile.controller;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import pl.wroc.pwr.agile.entity.Employee;
 import pl.wroc.pwr.agile.entity.Task;
+import pl.wroc.pwr.agile.entity.TaskType;
 import pl.wroc.pwr.agile.entity.UserStory;
 import pl.wroc.pwr.agile.service.UserService;
 import pl.wroc.pwr.agile.service.WorkspaceService;
@@ -70,27 +73,54 @@ public class WorkspaceController {
     
     @RequestMapping(value = "/planning/downloadPDF")
     public ModelAndView downloadExcel() {
+    	
         // create some sample data
     	
     	System.out.println("222");
-        List<Task> listTasks = new ArrayList<Task>();
+        Set<Task> listTasks = new HashSet<Task>();
+        List<UserStory> listStories = new ArrayList<UserStory>();
+        
+        UserStory story = new UserStory();
+        story.setNumber("US01");
+        story.setPoints("3");
+        story.setSummary("Jako użytkownik chcę się zalogować za pomocą Jiry");
+        story.setWorkspace(userService.getLoggedUser().getWorkspace());
+        
+        UserStory story2 = new UserStory();
+        story2.setNumber("US02");
+        story2.setPoints("6");
+        story2.setSummary("Jako user chce moc zmienic haslo");
+        story2.setWorkspace(userService.getLoggedUser().getWorkspace());
         
         Task t = new Task();
-        t.setSummary("qqqqqqqqqqqqqqq");
+        t.setType(TaskType.DEVELOPER_TASK);
+        t.setNumber("T1");
+        t.setEstimation(2D);
+        t.setSummary("Strona logowania");
         listTasks.add(t);
         
         Task t2 = new Task();
-        t2.setSummary("aaaaaaaaaaaaaaaaaaa");
+        t2.setType(TaskType.DEVELOPER_TASK);
+        t2.setNumber("T2");
+        t2.setEstimation(3D);
+        t2.setSummary("Logika strony logowania");
         listTasks.add(t2);
         
         
         Task t3 = new Task();
-        t3.setSummary("bbbbbbbbbbbbb");
+        t3.setType(TaskType.TESTER_TASK);
+        t3.setNumber("T3");
+        t3.setEstimation(5D);
+        t3.setSummary("Testy jednostkowe do logowania");
         listTasks.add(t);
+        
+        story.setTasks(listTasks);
+        listStories.add(story);
+        listStories.add(story2);
       
- 
+
         // return a view which will be resolved by an excel view resolver
-        return new ModelAndView("pdfView", "listBooks", listTasks);
+        return new ModelAndView("pdfView", "listStories", listStories);
     }
     
 }
