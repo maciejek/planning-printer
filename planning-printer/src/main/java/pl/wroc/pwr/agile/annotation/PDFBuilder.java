c30be3bb1c8b275571f451e0bed40a8e42d7844e
wposlednicka <wposlednicka@844e95ce-14d9-20eb-99b5-a5bf619f410c>
2015-01-06 22:16:23 +0000

    poprawa wyglądu pdf

--- planning-printer/src/main/java/pl/wroc/pwr/agile/annotation/PDFBuilder.java
+++ planning-printer/src/main/java/pl/wroc/pwr/agile/annotation/PDFBuilder.java
@@ -10,132 +10,129 @@ import javax.servlet.http.HttpServletResponse;
 import pl.wroc.pwr.agile.entity.Task;
 import pl.wroc.pwr.agile.entity.UserStory;
 
-import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.pdf.draw.LineSeparator;
- 
+
 /**
  * This view class generates a PDF document 'on the fly' based on the data
  * contained in the model.
+ * 
  * @author www.codejava.net
  *
  */
 public class PDFBuilder extends AbstractITextPdfView {
- 
-    @Override
-    protected void buildPdfDocument(Map<String, Object> model, Document doc,
-            PdfWriter writer, HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
-        // get data model which is passed by the Spring container
-        List<UserStory> listStories = (List<UserStory>) model.get("listStories");
-         
-        doc.add(new Paragraph("User stories:"));
-        
-        for (UserStory story : listStories) {
-			PdfPTable table = new PdfPTable(1);
-			table.setWidthPercentage(100.0f);
-		    table.setWidths(new float[] {5.0f});
-		    table.setSpacingBefore(10);
-		    
-		    Font font = FontFactory.getFont(FontFactory.TIMES);
-	        font.setColor(BaseColor.WHITE);
-	        
-	        PdfPCell cell = new PdfPCell();
-	        cell.setBackgroundColor(BaseColor.BLUE);
-	        cell.setPadding(5);
 
+	@Override
+	protected void buildPdfDocument(Map<String, Object> model, Document doc, PdfWriter writer, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// get data model which is passed by the Spring container
+		List<UserStory> listStories = (List<UserStory>) model.get("listStories");
 
-	        table.addCell(story.getNumber() + ".  " + story.getSummary() + " -- " +story.getPoints());
-	        doc.add(table);
-	        doc.add(new LineSeparator());
-	        
-	        Set<Task> tasks = story.getTasks();
-	        
-	        if (tasks !=null) {
-				
-	        for (Task task : tasks) {
-	        	PdfPTable table1 = new PdfPTable(1);
-	          table1.setWidthPercentage(100.0f);
-	          table1.setWidths(new float[] {3.0f});
-	          table1.setSpacingBefore(10);
-	           
-	          // define font for table header row
-	          Font fontTask = FontFactory.getFont(FontFactory.HELVETICA);
-	          fontTask.setColor(BaseColor.WHITE);
-	           
-	          // define table header cell
-	          PdfPCell cellTask = new PdfPCell();
-	          cellTask.setBackgroundColor(BaseColor.BLUE);
-	          cellTask.setPadding(5);
-	           
-	          // write table header
-	          cellTask.setPhrase(new Phrase("Book Title", font));
-	          table1.addCell(cell);
-	           
+		doc.add(new Paragraph("User stories:"));
 
-		        table1.addCell(task.getNumber() + ".  " + task.getSummary());
-		       
-		        
-		        PdfPTable table2 = new PdfPTable(2);
-		        table2.setWidthPercentage(100.0f);
-		          table2.setWidths(new float[] {2.0f, 2.0f});
-		          table2.setSpacingBefore(10);
-		          
-		          PdfPCell cellTask2 = new PdfPCell();
-		          cellTask2.setPadding(5);
-		           
-		          // write table header
-		          cellTask2.setPhrase(new Phrase("odpowiedzialny", font));
-		          table2.addCell(cellTask2);
-		          table2.addCell(new Phrase("ktoś kto go robi"));
-		          
-		          table1.addCell(table2);
-		        
-		        
-		        doc.add(table1);
-		        doc.add(new LineSeparator());
+		for (UserStory story : listStories) {
+			PdfPTable userStoryTable = createUserStoryTable(story);
+			doc.add(userStoryTable);
+			doc.add(new LineSeparator());
+
+			Set<Task> userStoryTasks = story.getTasks();
+
+			if (userStoryTasks != null) {
+
+				for (Task task : userStoryTasks) {
+					PdfPTable taskTable = createTaskTable(task);
+					doc.add(taskTable);
+					doc.add(new LineSeparator());
+				}
 			}
-	        }
-	        
 		}
-        
-        
-         
-//        PdfPTable table = new PdfPTable(5);
-//        table.setWidthPercentage(100.0f);
-//        table.setWidths(new float[] {3.0f, 2.0f, 2.0f, 2.0f, 1.0f});
-//        table.setSpacingBefore(10);
-//         
-//        // define font for table header row
-//        Font font = FontFactory.getFont(FontFactory.HELVETICA);
-//        font.setColor(BaseColor.WHITE);
-//         
-//        // define table header cell
-//        PdfPCell cell = new PdfPCell();
-//        cell.setBackgroundColor(BaseColor.BLUE);
-//        cell.setPadding(5);
-//         
-//        // write table header
-//        cell.setPhrase(new Phrase("Book Title", font));
-//        table.addCell(cell);
-//         
-//        table.addCell(cell);
-//         
-//        // write table row data
-//        for (Task aBook : listBooks) {
-//            table.addCell(aBook.getSummary());
-//            
-//        }
-//         
-//        doc.add(table);
-//         
-    }
- 
+		
+	}
+
+	private PdfPTable createTaskTable(Task task) throws DocumentException {
+
+		PdfPTable table = new PdfPTable(10);
+		table.setWidthPercentage(100.0f);
+		table.setWidths(new float[] { 3.0f, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f, 3.0f });
+		table.setSpacingBefore(10);
+
+		PdfPCell cellTask;
+		cellTask = new PdfPCell();
+		cellTask.setColspan(10);
+		cellTask.setPhrase(new Phrase(task.getNumber() + ".  "+ task.getSummary()));
+		cellTask.setHorizontalAlignment(Element.ALIGN_CENTER);
+		cellTask.setPaddingBottom(5);
+		table.addCell(cellTask);
+		table.completeRow();
+
+		cellTask = new PdfPCell();
+		cellTask.setColspan(2);
+		cellTask.setPhrase(new Phrase("Odpowiedzialny:"));
+		cellTask.setPaddingBottom(5);
+		table.addCell(cellTask);
+		cellTask.setColspan(8);
+		cellTask.setPhrase(new Phrase("Darek"));
+		table.addCell(cellTask);
+		table.completeRow();
+
+		cellTask = new PdfPCell();
+		cellTask.setColspan(2);
+		cellTask.setPhrase(new Phrase("Estymacja:"));
+		cellTask.setPaddingBottom(5);
+		table.addCell(cellTask);
+		cellTask.setColspan(8);
+		cellTask.setPhrase(new Phrase("5h"));
+		table.addCell(cellTask);
+		table.completeRow();
+
+		cellTask = new PdfPCell();
+		cellTask.setColspan(2);
+		cellTask.setPhrase(new Phrase("Spędzono:"));
+		cellTask.setPaddingBottom(5);
+		table.addCell(cellTask);
+		cellTask.setColspan(8);
+		cellTask.setPhrase(new Phrase("3h 30 min"));
+		cellTask.setPaddingBottom(5);
+		table.addCell(cellTask);
+		table.completeRow();
+
+		cellTask = new PdfPCell();
+		cellTask.setColspan(1);
+		cellTask.setPhrase(new Phrase("Dzień:"));
+		cellTask.setPaddingBottom(5);
+		table.addCell(cellTask);
+		table.completeRow();
+
+		cellTask = new PdfPCell();
+		cellTask.setColspan(1);
+		cellTask.setPhrase(new Phrase("Czas:"));
+		cellTask.setPaddingBottom(5);
+		table.addCell(cellTask);
+		table.completeRow();
+
+		return table;
+	}
+
+	private PdfPTable createUserStoryTable(UserStory story) throws DocumentException {
+		PdfPTable table = new PdfPTable(1);
+		table.setWidthPercentage(100.0f);
+		table.setWidths(new float[] { 5.0f });
+		table.setSpacingBefore(10);
+
+		PdfPCell cell = new PdfPCell();
+		cell.setPhrase(new Phrase(story.getNumber() + ".  " + story.getSummary()+ " -- " + story.getPoints()));
+		cell.setPadding(5);
+		cell.setPaddingBottom(5);
+		table.addCell(cell);
+		
+		return table;
+	}
+
 }
