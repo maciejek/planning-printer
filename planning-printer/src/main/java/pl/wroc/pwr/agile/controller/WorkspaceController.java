2c02d98010afcd605121c3c36bb92aea8f43e56f
wposlednicka <wposlednicka@844e95ce-14d9-20eb-99b5-a5bf619f410c>
2015-01-11 21:37:06 +0000

    poprawienie generowania pdf

--- planning-printer/src/main/java/pl/wroc/pwr/agile/controller/WorkspaceController.java
+++ planning-printer/src/main/java/pl/wroc/pwr/agile/controller/WorkspaceController.java
@@ -73,54 +73,9 @@ public class WorkspaceController {
     
     @RequestMapping(value = "/planning/downloadPDF")
     public ModelAndView downloadExcel() {
-    	
-        // create some sample data
-    	
-    	System.out.println("222");
-        Set<Task> listTasks = new HashSet<Task>();
-        List<UserStory> listStories = new ArrayList<UserStory>();
-        
-        UserStory story = new UserStory();
-        story.setNumber("US01");
-        story.setPoints("3");
-        story.setSummary("Jako użytkownik chcę się zalogować za pomocą Jiry");
-        story.setWorkspace(userService.getLoggedUser().getWorkspace());
-        
-        UserStory story2 = new UserStory();
-        story2.setNumber("US02");
-        story2.setPoints("6");
-        story2.setSummary("Jako user chce moc zmienic haslo");
-        story2.setWorkspace(userService.getLoggedUser().getWorkspace());
-        
-        Task t = new Task();
-        t.setType(TaskType.DEVELOPER_TASK);
-        t.setNumber("T1");
-        t.setEstimation(2D);
-        t.setSummary("Strona logowania");
-        listTasks.add(t);
-        
-        Task t2 = new Task();
-        t2.setType(TaskType.DEVELOPER_TASK);
-        t2.setNumber("T2");
-        t2.setEstimation(3D);
-        t2.setSummary("Logika strony logowania");
-        listTasks.add(t2);
-        
-        
-        Task t3 = new Task();
-        t3.setType(TaskType.TESTER_TASK);
-        t3.setNumber("T3");
-        t3.setEstimation(5D);
-        t3.setSummary("Testy jednostkowe do logowania");
-        listTasks.add(t);
-        
-        story.setTasks(listTasks);
-        listStories.add(story);
-        listStories.add(story2);
-      
-
-        // return a view which will be resolved by an excel view resolver
-        return new ModelAndView("pdfView", "listStories", listStories);
+       Set<UserStory> userStories = workspaceService.getCurrentWorkspace().getUserStories();
+       List<UserStory> listStories = new ArrayList<UserStory>(userStories);
+       return new ModelAndView("pdfView", "listStories", listStories);
     }
     
 }
