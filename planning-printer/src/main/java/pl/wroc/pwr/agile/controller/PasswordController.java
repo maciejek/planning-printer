4be2e07450e9c1db0f69973bbdfaf8c13fc47013
maciekradoszko <maciekradoszko@gmail.com>
2015-01-21 10:31:45 +0000

    change password correction

--- planning-printer/src/main/java/pl/wroc/pwr/agile/controller/PasswordController.java
+++ planning-printer/src/main/java/pl/wroc/pwr/agile/controller/PasswordController.java
@@ -5,11 +5,13 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import pl.wroc.pwr.agile.entity.User;
 import pl.wroc.pwr.agile.service.UserService;
 
 @Controller
@@ -17,6 +19,11 @@ public class PasswordController {
 
     @Autowired
     private UserService userService;
+    
+    @ModelAttribute("deputy")
+    public User construct() {
+        return new User();
+    }
     
     @RequestMapping("/passwordCorrect")
     @ResponseBody
